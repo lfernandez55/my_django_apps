e23d6203d164f3755ac9ff609bee4e1f54d2096e
lfernandez55 <lfernandez@weber.edu>
2019-02-23 00:29:16 -0700

    Working except for button that adds more tasks.  See desc
    This is a bug in the plugin or maybe in the way I have it configure.  I can always replace his library with my own custom script.

--- task/urls.py
+++ task/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('taskgroup/add/', views.TaskgroupTaskCreate.as_view(), name='taskgroup_add'),
     path('taskgroup/<int:pk>/', views.TaskgroupTaskUpdate.as_view(), name='taskgroup_update'),
     path('taskgroup/<int:pk>/delete/', views.TaskgroupDelete.as_view(), name='taskgroup_delete'),
+    path('foo/', views.foo, name='foo'),
+    # path('foo/',views.Foo.as_view(), name='foo'),
 ]
--- task/views.py
+++ task/views.py
@@ -68,10 +68,12 @@ class TaskgroupTaskUpdate(UpdateView):
         if self.request.POST:
             data['tasks'] = TaskFormSet(self.request.POST, instance=self.object)
         else:
-            # querytasks = Task.objects.all().order_by('-sequence')
-            # querytasks = TaskGroup.objects.all().filter(author=self.request.user).order_by('-task_group_name')
-            # data['tasks'] = TaskFormSet(queryset=querytasks)
-            data['tasks'] = TaskFormSet(instance=self.object)
+            # Notice here that the "self.object" contains a string like "foo" or whatever
+            #the actual name of the TaskGroup is.
+            #if one doesn't pass in the queryset the sequence wont be ordered.  This is
+            #the only reason it's being passed in.
+            querytasks = Task.objects.filter(Task_group__task_group_name=self.object).order_by('-sequence')
+            data['tasks'] = TaskFormSet(instance=self.object,queryset=querytasks)
 
         return data
 
@@ -86,12 +88,38 @@ class TaskgroupTaskUpdate(UpdateView):
                 tasks.save()
         return super().form_valid(form)
 
+def foo(request):
+    #there doesn't seem to be any difference between what is output by q1 or q2 even
+    #though their sql is different when it is dumped out with the print statments below.
+    #the sql join is the same. what is different in the sql is the fields that are specified
+    #directly after the select statement.  oddly the user email field isn't listed and yet
+    #it can still be outputted in the template!
+    q1 = Task.objects.select_related('Task_group').filter(Task_group__task_group_name='Foo')
+    q2 = Task.objects.filter(Task_group__task_group_name='Foo')
+    #q2 = TaskGroup.objects.select_related('author')
+    print(q1.query)
+    print('-------------------------------------------------------------------')
+    print(q2.query)
+    print('-------------------------------------------------------------------')
+    print(q1)
+    print('-------------------------------------------------------------------')
+    print(q2)
+
+    return render(request, 'task/foo.html', {'q1':q1,'q2':q2 })
+
+# def foo(request):
+#     myString="foo"
+#     print('dddddddddddddddddddddebug')
+#     phrases = ['three french hens','two turtle doves','a partridge in a pear tree']
+#     return TemplateResponse(request, 'u2/foo.html', { 'myString': myString, 'phrases': phrases })
 
 
 class TaskgroupDelete(DeleteView):
     model = TaskGroup
     success_url = reverse_lazy('taskgroup_list')
 
+
+    # return render(request, foo.html, {'your_queryset':your_queryset})
 # these might come in handy in the future if i want to move to function based views
 # def TaskgroupDelete(request, taskgroup_id, template_name='task/taskgroup_confirm_delete.html'):
 #     taskgroup= get_object_or_404(TaskGroup, pk=taskgroup_id)
