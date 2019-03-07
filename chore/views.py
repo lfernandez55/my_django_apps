5d4bc16704dc318fe547638a7a3e17cc8a14c761
lfernandez55 <lfernandez@weber.edu>
2019-03-06 20:08:33 -0700

    Half way through making chore updates

--- chore/views.py
+++ chore/views.py
@@ -34,11 +34,42 @@ def choregroupCreate(request):
                 )
         return redirect('choregroup_list')
     else:
-        return render(request, 'chore/choregroup_form.html')
+        return render(request, 'chore/choregroup_create.html')
 
 
-def choregroupUpdate(request):
+def choregroupUpdate(request, pk):
     print('debug choreGroupUpdate')
+    if request.method == 'POST':
+        print(request.POST['choregroup_name'])
+        print(request.POST['description'])
+        print(request.POST)
+        # newchoreGroup = ChoreGroup.objects.create(
+        #     choregroup_name=request.POST['choregroup_name'],
+        #     description=request.POST['description'],
+        #     author=request.user
+        # )
+        # print('new choregroupid:', newchoreGroup.id)
+        # for formName in request.POST:
+        #     print(formName, request.POST[formName])
+        #     if formName.startswith('chore_name'):
+        #         choreNumber=formName.split("_")[2]
+        #         sequenceField= 'chore_sequence_' + choreNumber
+        #         Chore.objects.create(
+        #             chore_name=request.POST[formName],
+        #             sequence=request.POST[sequenceField],
+        #             choregroup=newchoreGroup
+        #         )
+        return redirect('choregroup_list')
+    else:
+        choregroup = ChoreGroup.objects.all().values('choregroup_name','description','Chore__chore_name').filter(pk=pk)
+        # print(choregroup.description)
+        print(choregroup.query)
+        for ddd in choregroup:
+            print(ddd)
+            print(ddd['choregroup_name'])
+            print(ddd['description'])
+            print(ddd['Chore__chore_name'])
+        return render(request, 'chore/choregroup_update.html', {'choregroup':choregroup } )
 
 def choregroupDelete(request,pk):
     ChoreGroup.objects.filter(pk=pk).delete()
