9dd9aaef6118fced5206de18481e31b683156f83
lfernandez55 <lfernandez@weber.edu>
2019-03-07 20:28:10 -0700

    Almost finished updateform
    Can update existing chores
    TODO:  see whether can add new chore
    TODO:  Get remove working

--- chore/views.py
+++ chore/views.py
@@ -23,7 +23,7 @@ def choregroupCreate(request):
         )
         print('new choregroupid:', newchoreGroup.id)
         for formName in request.POST:
-            print(formName, request.POST[formName])
+            print('ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ',formName, request.POST[formName])
             if formName.startswith('chore_name'):
                 choreNumber=formName.split("_")[2]
                 sequenceField= 'chore_sequence_' + choreNumber
@@ -38,37 +38,45 @@ def choregroupCreate(request):
 
 
 def choregroupUpdate(request, pk):
-    print('debug choreGroupUpdate')
+    print('debug choreGroupUpdate', pk)
     if request.method == 'POST':
-        print(request.POST['choregroup_name'])
-        print(request.POST['description'])
-        print(request.POST)
-        # newchoreGroup = ChoreGroup.objects.create(
-        #     choregroup_name=request.POST['choregroup_name'],
-        #     description=request.POST['description'],
-        #     author=request.user
-        # )
-        # print('new choregroupid:', newchoreGroup.id)
-        # for formName in request.POST:
-        #     print(formName, request.POST[formName])
-        #     if formName.startswith('chore_name'):
-        #         choreNumber=formName.split("_")[2]
-        #         sequenceField= 'chore_sequence_' + choreNumber
-        #         Chore.objects.create(
-        #             chore_name=request.POST[formName],
-        #             sequence=request.POST[sequenceField],
-        #             choregroup=newchoreGroup
-        #         )
+        #update the choregroup
+        choregroup = ChoreGroup.objects.get(pk=pk)
+        choregroup.choregroup_name = request.POST['choregroup_name']
+        choregroup.description = request.POST['description']
+        choregroup.save()
+
+        for formField in request.POST:
+            #update (or delete) existing chores
+            if formField.startswith('chore_name'):
+                choreId=formField.split("_")[2]
+                chore = Chore.objects.get(pk=choreId)
+                chore.chore_name=request.POST[formField]
+                sequence='chore_sequence_' + choreId
+                chore.sequence=request.POST[sequence]
+                if request.POST[formField] == '':
+                    chore.delete()
+                else:
+                    chore.save()
+            #TODO add new chores
+            if formField.startswith('newchore_name'):
+                choreId=formField.split("_")[2]
+                sequence='newchore_sequence_' + choreId
+                newChore = Chore(chore_name=request.POST[formField], sequence=sequence)
+                newChore.save()
+                chore.chore_name=request.POST[formField]
+
         return redirect('choregroup_list')
     else:
-        choregroup = ChoreGroup.objects.all().values('choregroup_name','description','Chore__chore_name').filter(pk=pk)
+        choregroup = ChoreGroup.objects.all().values('choregroup_name','description','Chore__chore_name','Chore__id','Chore__sequence').filter(pk=pk).order_by('-Chore__sequence')
+        # choregroup = Chore.objects.all().values('chore_name','sequence','choregroup__choregroup_name','choregroup__description').filter(pk=pk)
         # print(choregroup.description)
         print(choregroup.query)
         for ddd in choregroup:
             print(ddd)
             print(ddd['choregroup_name'])
             print(ddd['description'])
-            print(ddd['Chore__chore_name'])
+            print(ddd['Chore__chore_name'],ddd['Chore__id'],ddd['Chore__sequence'])
         return render(request, 'chore/choregroup_update.html', {'choregroup':choregroup } )
 
 def choregroupDelete(request,pk):
